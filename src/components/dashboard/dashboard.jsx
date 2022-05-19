19e4017e6f96587a5690e6c5918f8322143f58f2
Louison <louison59279@gmail.com>
2022-05-19 18:18:27 +0200

    final push

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,5 +1,10 @@
-import React, {useEffect, useState} from "react"
-import { useParams } from "react-router"
+import React, {
+  useEffect,
+  useState
+} from "react"
+import {
+  useParams
+} from "react-router"
 import styles from './dashboard.module.css'
 import Resume from '../resume/resume'
 import BarCharts from '../graphs/barchart/barchart'
@@ -17,52 +22,52 @@ import Error404 from "../Error404/Error404"
  * @returns {JSX.Element}
  */
 
-function Dashboard () {
-    const [user, setUser] = useState(undefined)
-    let { id } = useParams()
-    // if URL param is not user 12 or user 18, then display Error 404 component
-    if (parseInt(id) !== 18 && parseInt(id) !== 12){
-        return <Error404 />
-    }
+function Dashboard() {
+  const [user, setUser] = useState(undefined)
+  let {id} = useParams()
+  // if URL param is not user 12 or user 18, then display Error 404 component
+  if (parseInt(id) !== 18 && parseInt(id) !== 12) {
+    return <Error404 />
+  }
 
-    useEffect(() => {
-      // if API is enabled, fetch it and set datas, if it's not : switch between datas12 and datas18 mock files. 
-      // eslint-disable-next-line no-undef
-      if (process.env.REACT_APP_SOURCE === "API") {
-        ApiServices(id).then(datas => {
-          setUser(datas)
-        })
-      } else {
-        switch (parseInt(id)){
-          case 12 :
+  useEffect(() => {
+    // if API is enabled, fetch it and set datas, if it's not : switch between datas12 and datas18 mock files. 
+    // eslint-disable-next-line no-undef
+    if (process.env.REACT_APP_SOURCE === "API") {
+      ApiServices(id).then(datas => {
+        setUser(datas)
+      })
+    } else {
+      switch (parseInt(id)) {
+        case 12:
           setUser(datas12)
           break
-          case 18 : 
+        case 18:
           setUser(datas18)
           break
-          default :
+        default:
           return <Error404 />
-        }
       }
-    }, [])
+    }
+  }, [])
     
-    return ( user ? 
-        <div className={styles.dashboard}>
-        <div>
-            <span className={styles.user}>Bonjour</span>
-            <span className={`${styles.user} ${styles.name}`}>{user[0].userInfos.firstName}</span>
-            <p className={styles.congratulations}>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
-        </div>
-            <BarCharts data={user}></BarCharts>
-            <Resume data={user}></Resume>
-                <div className={styles.graphs}>
-                    <Average data={user}></Average>
-                    <RadarGraph data={user}></RadarGraph>
-                    <Score data={user}></Score>
-                </div>
-        </div> : <div className={styles.loadingCtn}>
-                   <h1 className={styles.loading}>Chargement...</h1>
-                 </div>
+  return (user ?
+    <div className={styles.dashboard}>
+      <div>
+        <span className={styles.user}>Bonjour</span>
+        <span className={`${styles.user} ${styles.name}`}>{user[0].userInfos.firstName}</span>
+        <p className={styles.congratulations}>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+      </div>
+      <BarCharts data={user}></BarCharts>
+      <Resume data={user}></Resume>
+      <div className={styles.graphs}>
+        <Average data={user}></Average>
+        <RadarGraph data={user}></RadarGraph>
+        <Score data={user}></Score>
+      </div>
+    </div> : <div className={styles.loadingCtn}>
+      <h1 className={styles.loading}>Chargement...</h1>
+    </div>
     )
 }
 
